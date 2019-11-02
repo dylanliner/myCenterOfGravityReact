5ccf7ae54fee1b7d59272fbeb8e365f321eba9bb
Dylan <dylanliner@gmail.com>
2019-11-02 19:26:59 +0100

    feat: formik form

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -3,6 +3,13 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 import React from "react";
 import { useDropzone } from 'react-dropzone';
 import oboe from 'oboe';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+import { Formik } from 'formik';
+import * as yup from 'yup'; // for everything
+import InputGroup from 'react-bootstrap/InputGroup'
+import Col from 'react-bootstrap/Col'
 
 
 
@@ -125,7 +132,22 @@ function Example({ onComplete, handleClose }) {
     });
 
     const isFileTooLarge = rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+
+
+
+    const schema = yup.object({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        username: yup.string().required(),
+        city: yup.string().required(),
+        state: yup.string().required(),
+        zip: yup.string().required(),
+        terms: yup.bool().required(),
+    });
+
+
     return (
+
         <>
 
             <Modal show={true} onHide={handleClose} >
@@ -133,38 +155,165 @@ function Example({ onComplete, handleClose }) {
                     <Modal.Title>What's my center of Gravity?</Modal.Title>
 
                 </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        validationSchema={schema}
+                        onSubmit={console.log}
+                        initialValues={{
+                            firstName: 'Mark',
+                            lastName: 'Otto',
+                        }}
+                    >
+                        {({
+                            handleSubmit,
+                            handleChange,
+                            handleBlur,
+                            values,
+                            touched,
+                            isValid,
+                            errors,
+                        }) => (
+                                <Form noValidate onSubmit={handleSubmit}>
+                                    <Form.Row>
+                                        <Form.Group as={Col} md="4" controlId="validationFormik01">
+                                            <Form.Label>First name</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                name="firstName"
+                                                value={values.firstName}
+                                                onChange={handleChange}
+                                                isValid={touched.firstName && !errors.firstName}
+                                            />
+                                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group as={Col} md="4" controlId="validationFormik02">
+                                            <Form.Label>Last name</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                name="lastName"
+                                                value={values.lastName}
+                                                onChange={handleChange}
+                                                isValid={touched.lastName && !errors.lastName}
+                                            />
 
-                < div className="container text-center mt-5" >
-                    <div {...getRootProps()} >
-                        <input {...getInputProps()} />
-                        {!isDragActive && 'Click here or drop a file to upload!'
-                        }
-                        {isDragActive && !isDragReject && "Drop it like it's hot!"}
-                        {isDragReject && "File type not accepted, sorry!"}
-                        {
-                            isFileTooLarge && (
-                                <div className="text-danger mt-2" >
-                                    File is too large.
+                                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group as={Col} md="4" controlId="validationFormikUsername">
+                                            <Form.Label>Username</Form.Label>
+                                            <InputGroup>
+                                                <InputGroup.Prepend>
+                                                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                                </InputGroup.Prepend>
+                                                <Form.Control
+                                                    type="text"
+                                                    placeholder="Username"
+                                                    aria-describedby="inputGroupPrepend"
+                                                    name="username"
+                                                    value={values.username}
+                                                    onChange={handleChange}
+                                                    isInvalid={!!errors.username}
+                                                />
+                                                <Form.Control.Feedback type="invalid">
+                                                    {errors.username}
+                                                </Form.Control.Feedback>
+                                            </InputGroup>
+                                        </Form.Group>
+                                    </Form.Row>
+                                    <Form.Row>
+                                        <Form.Group as={Col} md="6" controlId="validationFormik03">
+                                            <Form.Label>City</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="City"
+                                                name="city"
+                                                value={values.city}
+                                                onChange={handleChange}
+                                                isInvalid={!!errors.city}
+                                            />
+
+                                            <Form.Control.Feedback type="invalid">
+                                                {errors.city}
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group as={Col} md="3" controlId="validationFormik04">
+                                            <Form.Label>State</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="State"
+                                                name="state"
+                                                value={values.state}
+                                                onChange={handleChange}
+                                                isInvalid={!!errors.state}
+                                            />
+                                            <Form.Control.Feedback type="invalid">
+                                                {errors.state}
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group as={Col} md="3" controlId="validationFormik05">
+                                            <Form.Label>Zip</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Zip"
+                                                name="zip"
+                                                value={values.zip}
+                                                onChange={handleChange}
+                                                isInvalid={!!errors.zip}
+                                            />
+
+                                            <Form.Control.Feedback type="invalid">
+                                                {errors.zip}
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Form.Row>
+                                    <Form.Group>
+                                        <Form.Check
+                                            required
+                                            name="terms"
+                                            label="Agree to terms and conditions"
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.terms}
+                                            feedback={errors.terms}
+                                            id="validationFormik0"
+                                        />
+                                    </Form.Group>
+                                    <Button type="submit">Submit form</Button>
+                                </Form>
+                            )}
+                    </Formik>
+
+                    < div className="container text-center mt-5" >
+                        <div {...getRootProps()} >
+                            <input {...getInputProps()} />
+                            {!isDragActive && 'Click here or drop a file to upload!'
+                            }
+                            {isDragActive && !isDragReject && "Drop it like it's hot!"}
+                            {isDragReject && "File type not accepted, sorry!"}
+                            {
+                                isFileTooLarge && (
+                                    <div className="text-danger mt-2" >
+                                        File is too large.
           </div>
-                            )
+                                )
+                            }
+                        </div>
+                        < ul className="list-group mt-2" >
+                            {
+                                acceptedFiles.length > 0 && acceptedFiles.map(acceptedFile => (
+                                    <li className="list-group-item list-group-item-success" >
+                                        {acceptedFile.name}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div>
+                        {percentLoaded > 0 &&
+                            <ProgressBar animated now={percentLoaded} label={`${percentLoaded}%`} />
                         }
                     </div>
-                    < ul className="list-group mt-2" >
-                        {
-                            acceptedFiles.length > 0 && acceptedFiles.map(acceptedFile => (
-                                <li className="list-group-item list-group-item-success" >
-                                    {acceptedFile.name}
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-                <div>
-                    {percentLoaded > 0 &&
-                        <ProgressBar animated now={percentLoaded} label={`${percentLoaded}%`} />
-                    }
-                </div>
-
+                </Modal.Footer>
             </Modal>
         </>
     );
